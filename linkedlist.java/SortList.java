2c3089cbc87d0ec8b9acd3b90539cc758e52a5a0
Siridasari54 <24b01a1230@svecw.edu.in>
2026-03-18 21:32:24 +0530

    leetcode sortlist code

---
+++ linkedlist.java/SortList.java
@@ -0,0 +1,88 @@
+import java.util.*;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+class Solution {
+        public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode mid = slow.next;
+        slow.next = null; 
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (l1 != null) temp.next = l1;
+        if (l2 != null) temp.next = l2;
+
+        return dummy.next;
+    }
+}
+public class SortList {
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt(); 
+        if (n == 0) {
+            System.out.println("Empty list");
+            sc.close();
+            return;
+        }
+
+        ListNode head = new ListNode(sc.nextInt());
+        ListNode temp = head;
+
+        for (int i = 1; i < n; i++) {
+            temp.next = new ListNode(sc.nextInt());
+            temp = temp.next;
+        }
+
+        Solution sol = new Solution();
+        head = sol.sortList(head);
+
+        System.out.print("Sorted list: ");
+        printList(head);
+
+        sc.close();
+    }
+}
